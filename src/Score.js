562dd5c0bb7a0db9093fc6d57627d5bb76b928f7
Austin <higham5260@uwec.edu>
2020-05-16 20:02:31 -0500

    added placeholder to score submission

--- src/Score.js
+++ src/Score.js
@@ -20,7 +20,7 @@ export function Score(props) {
     if(score.isPosting){
         return (
             <div className="score">
-                <input type="text" value={name} onChange={e =>
+                <input type="text" placeholder="Enter your name..." value={name} onChange={e =>
                 setName(e.target.value)}/>
                 <input type="text" value={moves} readOnly/>
                 <button onClick={onSave}>Save Score</button>
